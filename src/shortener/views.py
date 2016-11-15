74baeca366e9d9839f53ce177f2a1ba0a748a7a1
Belonias <iakovosbelonias@gmail.com>
2016-11-15 17:36:55 +0200

    404 error msg

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .models import KirrURL
 # Create your views here.
 def kirr_redirect_view(request, shortcode=None, *args, **kwargs): #function based view FBV
-    #print(request.user)
-    #print(request.user.is_authenticated())\
-    return HttpResponse("hello {sc}".format(sc=shortcode))
+    obj = get_object_or_404(KirrURL, shortcode=shortcode)
+    return HttpResponse("hello {sc}".format(sc=obj.url))
 
 
 class KirrCBView(View): #class based view
     def get(self, request, shortcode=None, *args, **kwargs):
+        obj = get_object_or_404(KirrURL, shortcode=shortcode)
         return HttpResponse("hello again {sc}".format(sc=shortcode))
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse()
