61e23a29190927008fdd5f20920ead7fd8a3b067
Belonias <iakovosbelonias@gmail.com>
2016-11-17 19:27:54 +0200

    Django Forms

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -2,21 +2,30 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .forms import SubmitUrlForm
 from .models import KirrURL
 # Create your views here.
 
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "shortener/home.html", {})
+        the_form = SubmitUrlForm()
+        context = {
+            "title": "Submit URL context dictionary",
+            "form": the_form
+        }
+        return render(request, "shortener/home.html", context)
 
     def post(self, request, *args, **kwargs):
-        some_dict = {}
-        some_dict.get('url')
-        print(request.POST)
-        print(request.POST["url"])
-        print(request.POST.get("url"))
-        return render(request,"shortener/home.html", {})
+        form = SubmitUrlForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        context = {
+            "title": "Kirr.com",
+            "form": form,
+
+        }
+        return render(request,"shortener/home.html", context)
 
 
 
