0f5165ea8d09ed8b8fa55cceb90dd88bc3d3d905
Belonias <iakovosbelonias@gmail.com>
2016-12-01 13:13:20 +0200

    unit test

--- src/analytics/tests.py
+++ src/analytics/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from .models import ClickEvent
 # Create your tests here.
+# ./manage.py test shortener -v 2
+
+class ClickEventTest(TestCase):
+
+    def test_str(self):
+        my_title = ClickEvent(url='www.example.com')
+        self.assertEquals(
+            str(my_title), 'www.example.com'
+        )
