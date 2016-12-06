9bf44f51e262c8192edf3daa854efc4c4b87ed88
Belonias <iakovosbelonias@gmail.com>
2016-12-06 18:48:39 +0200

    validator.py

--- src/shortener/validators.py
+++ src/shortener/validators.py
@@ -4,16 +4,26 @@ from django.core.validators import URLValidator
 
 def validate_url(value):
     url_validator = URLValidator()
-    reg_val = value
-    if "http" in reg_val:
-        new_value = reg_val
-    else:
-        new_value = 'http://' + value
+    value_1_invalid = False
+    value_2_invalid = False
+    print(value)
     try:
-        url_validator(new_value)
+        url_validator(value)
+        print("reg working")
     except:
+        value_1_invalid = True
+    value_2_url = "http://" + value
+    try:
+        url_validator(value_2_url)
+        print("reg not working")
+    except:
+
+        value_2_invalid = True
+
+    print( value_1_invalid == False and value_2_invalid == False)
+    if value_1_invalid == False and value_2_invalid == False:
         raise ValidationError("Invalid URL for this field")
-    return new_value
+    return value
 
 
 def validate_dot_com(value):
